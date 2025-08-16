818a7ad8140e5b65af4558909f9c92d34603ad1f
Gokilavani1270 <81812056+Gokilavani1270@users.noreply.github.com>
2025-08-16 01:30:45 +0000

    CRUD applications done

---
+++ blog/routers/user.py
@@ -0,0 +1,23 @@
+from typing import List
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from .. import schemas, database, models
+from ..hashing import Hash
+from ..repository import user
+
+
+router = APIRouter(
+    prefix="/user",
+    tags = ['Users']
+)
+
+get_db = database.get_db
+
+
+@router.post('/', response_model=schemas.ShowUser)
+def create(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create(request, db)
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id:int, db:Session = Depends(get_db)):
+    return user.get_user(id, db)
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app=FastAPI()
 
 @app.get('/')
 def index():
 	return {'data': {'name':'vani'}}
+
+@app.get('/blog/{id}')
+def show(id):
+    return f'{id}'
+
+class Blog(BaseModel):
+    title:str
+    body:str
+
+@app.post('/blog')
+def create_blog(request:Blog):
+    return request
+
+
